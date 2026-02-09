0b83c58c7e8c6fc1e54d6ba67a046b82b4d4cd67
Agasthi16 <arunaagasthi@gmail.com>
2026-02-10 03:06:46 +0530

    add auth layout

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from "react-native"
+import React from "react"
+import { Slot, Stack } from "expo-router"
+
+const AuthLayout = () => {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        animation: "slide_from_right"
+      }}
+    >
+      
+    </Stack>
+  )
+}
+
+export default AuthLayout
